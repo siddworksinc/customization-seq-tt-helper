f6ddee489e8302b74ca61604189008e58d06ece3
siddworksinc <siddworks.inc@gmail.com>
2016-10-20 01:09:30 +0530

    Update calculate.js

--- javascripts/calculate.js
+++ javascripts/calculate.js
@@ -805,46 +805,6 @@ var newZeroes = function(length) {
   return Array.apply(null, new Array(length)).map(Number.prototype.valueOf,0);
 };
 
-// Pass in the objects to merge as arguments.
-// For a deep extend, set the first argument to `true`.
-var extend = function () {
-
-    // Variables
-    var extended = {};
-    var deep = false;
-    var i = 0;
-    var length = arguments.length;
-
-    // Check if a deep merge
-    if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
-        deep = arguments[0];
-        i++;
-    }
-
-    // Merge the object into the extended object
-    var merge = function (obj) {
-        for ( var prop in obj ) {
-            if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
-                // If deep merge and property is an object, merge properties
-                if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
-                    extended[prop] = extend( true, extended[prop], obj[prop] );
-                } else {
-                    extended[prop] = obj[prop];
-                }
-            }
-        }
-    };
-
-    // Loop through each object and conduct a merge
-    for ( ; i < length; i++ ) {
-        var obj = arguments[i];
-        merge(obj);
-    }
-
-    return extended;
-
-};
-
 var addArtifacts = function(world, bonuses, artifacts) {
   artifacts.forEach(function(a, i) {
     var id = a[0];
@@ -903,7 +863,7 @@ var levelToSkills = function(world, level) {
 
 
 var copyArray = function(array) {
-  return extend(true, [], array);
+  return $.extend(true, [], array);
 };
 
 var EMPTY_BONUSES = Object.keys(BonusTypes).map(function(b) { return 0; });
